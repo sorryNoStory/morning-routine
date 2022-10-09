c239fd6aadb89bb6f37d68c27adac8205a0c4538
Медуша <Medusa@MacBook-Pro-4.local>
2022-10-09 13:09:06 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Mroutine } from './Mroutine';
+
+function App() {
+    return(
+        <div className='app'>
+            <div className='content'>
+            <h1>My morning routine 🧚🏻‍♀️</h1>
+            <Mroutine />
+            </div>
+        </div>
+    )
+
+}
+
+export default App;
